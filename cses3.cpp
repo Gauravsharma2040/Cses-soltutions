439e2cb7a0b60a8279f06ffd396e65027fb26826
Gauravsharma2040 <gauravsharma6420@gmail.com>
2025-10-28 18:23:24 +0530

    Update cses3.cpp

--- cses3.cpp
+++ cses3.cpp
@@ -1,24 +1,26 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <cstring>
 using namespace std;
-int main(){
-    char str[100000000];
-    cin>>str;
-    int n=strlen(str);
-    int k=1;
-    int j=0;
-    for(int i=0;i<=n-1;i++){
-        if(str[i] == str[i+1]){
+
+int main() {
+    char str[1000001]; // 1e6 is plenty for this problem
+    cin >> str;
+    int n = strlen(str);
+
+    int k = 1; // current repetition length
+    int j = 1; // max repetition length
+
+    for (int i = 0; i < n - 1; i++) {
+        if (str[i] == str[i + 1]) {
             k++;
+        } else {
+            k = 1;
         }
-        else{
-            k=1;
-            }
-        if(k>j){
-            j=k;
-            
+        if (k > j) {
+            j = k;
         }
     }
-    cout<<j;
+
+    cout << j;
     return 0;
 }
