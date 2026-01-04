a812614fa5264e1f11f1be0a4e17527e0ca9c22f
ini <gauravsharma6420@gmail.com>
2026-01-04 20:51:12 +0530

    dp2

---
+++ dynamic programing/dp2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+return 0;
+int n,x;
+cin>>n>>x;
+vector<int> coins(n);
+vector<int> dp(x+1,INT_MAX);
+dp[0] = 0;
+for(int i=0;i<n;i++) cin>>coins[i];
+for(int i=1;i<=x;i++){
+    for(auto c : coins){
+        if(i-c <0 || dp[i-c] == INT_MAX) continue;
+        dp[i] = min(dp[i],dp[i-c]+1);
+    }
+}
+dp[x] == INT_MAX?cout<<-1:cout<<dp[x];
+}
--- sortnsearch/sns3.cpp
+++ sortnsearch/sns3.cpp
@@ -4,10 +4,8 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int n, x;
-    cin >> n >> x;
-
+    cin>>n>>x;
     vector<int> w(n);
     for (int i = 0; i < n; i++) cin >> w[i];
     sort(w.begin(), w.end());
