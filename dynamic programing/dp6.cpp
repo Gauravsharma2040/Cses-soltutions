fedc03619c69e8e8d2166f7ba8dd4a0fab940f4b
ini <gauravsharma6420@gmail.com>
2026-01-06 19:17:56 +0530

    dp 6 & 7

---
+++ dynamic programing/dp6.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+const static long long MOD = 1e9+7;
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int n;
+cin>>n;
+vector<vector<char>> grid(n,vector<char>(n));
+for(int i=0;i<n;i++) for(int j=0;j<n;j++) cin>>grid[i][j];
+vector<vector<int>> dp(n,vector<int> (n,0));
+dp[0][0] = grid[0][0] == '*' ? 0 : 1;
+if(dp[0][0] == 0) {
+    cout<<0; 
+    return 0;
+}
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+     if (i == 0 && j == 0) continue;
+     if(grid[i][j] == '*') dp[i][j] = 0;
+     else{
+            long long ways = 0;
+            if (i > 0) ways += dp[i-1][j];
+            if (j > 0) ways += dp[i][j-1];
+            dp[i][j] = ways % MOD;
+        }
+    }
+}
+cout<<dp[n-1][n-1]%MOD;
+return 0;
+}
