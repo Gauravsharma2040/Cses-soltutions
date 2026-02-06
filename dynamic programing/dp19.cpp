f6035b66800a599175633d378170d9e1ddf3d895
ini <gauravsharma6420@gmail.com>
2026-02-06 18:53:58 +0530

    dp19

--- dynamic programing/dp19.cpp
+++ dynamic programing/dp19.cpp
@@ -1,13 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
-int n;
-cin>>n;
-vector<vector<int>> project(n,vector<int>(3));
-for(int i =0;i<n;i++) for(int j=0;j<3;j++) cin>>project[i][j];
- 
-
-return 0;
+struct Project {
+    long long s, e, p;
+};
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<Project> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i].s >> a[i].e >> a[i].p;
+    }
+    sort(a.begin(), a.end(), [](const Project& x, const Project& y) {
+        return x.e < y.e;
+    });
+    vector<long long> ends(n);
+    for (int i = 0; i < n; ++i) ends[i] = a[i].e;
+    vector<long long> dp(n, 0);
+    for (int i = 0; i < n; ++i) {
+        long long take = a[i].p;
+        int j = lower_bound(ends.begin(), ends.end(), a[i].s) - ends.begin() - 1;
+        if (j >= 0) take += dp[j];
+        long long skip = (i > 0 ? dp[i-1] : 0);
+        dp[i] = max(skip, take);
+    }
+    cout << dp[n-1] << "\n";
+    return 0;
 }
